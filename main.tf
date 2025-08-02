637004a1fe7368f831d122d55b6d9f9d84d67062
= <=>
2025-08-02 16:34:12 +0530

    .

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "ec2" {
   source         = "./ec2_instance"
-  ami_value      = "ami-0e86e20dae9224db8"
+  ami_value      = "ami-020cba7c55df1f615"
   instance_type  = "t2.micro"
   subnet         = "subnet-0bcd2a69b1fc1fead"
   instance_count = var.instance_count
